7eaffef493e21d3fdf5ff2ba934d348d259d813d
drkrssll <russellderek01@gmail.com>
2024-10-09 19:57:47 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Chunks-rs
+
+A library that simplifies the process of making widgets for Unix operating systems.
+
+Uses GTK4 and GTK4 Layer Shell at its core, for ease of use with both X11 and Wayland Window Managers.
+
+```toml
+[dependencies]
+gtk4 = "0.9.2"
+gtk4-layer-shell = "0.4.0"
+chunks-rs = "0.1.0"
+```
+
+```rs
+const STYLE: &str = "
+window {
+    background-color: transparent;
+}
+
+#clock_label {
+    font-size: 34px;
+    background-color: #000000;
+    color: #FFFFFF;
+}
+";
+
+fn main() -> ExitCode {
+    let factory = Factory::new("chunk.factory");
+
+    let chunks = move |factory: &Application| {
+        let clock_tag = Chunk::tag("clock_label");
+        let clock_margins = vec![(Edge::Top, 20), (Edge::Right, 20)];
+        let clock_edge = EdgeConfig::TOP_RIGHT.to_vec();
+
+        Internal::handle_time(&clock_tag);
+        Internal::load_css(STYLE);
+
+        Chunk::new(
+            factory,
+            "Clock",
+            clock_tag,
+            clock_margins,
+            clock_edge,
+            Layer::Overlay,
+        );
+    };
+
+    factory.pollute(chunks)
+}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -8,133 +8,123 @@ use std::{
 };
 
 use chrono::Local;
-use gio::{
-    glib::{clone::Downgrade, timeout_add_local, ControlFlow},
-    prelude::IsA,
-};
+use gio::glib::{clone::Downgrade, timeout_add_local, ControlFlow};
 use gtk4::{
     gdk::Display,
     glib::{timeout_add_seconds_local, ExitCode},
-    prelude::{ApplicationExtManual, WidgetExt},
+    prelude::{ApplicationExt, ApplicationExtManual, GtkWindowExt, WidgetExt},
     style_context_add_provider_for_display, Application, ApplicationWindow, CssProvider, Label,
-    Widget, STYLE_PROVIDER_PRIORITY_APPLICATION,
+    STYLE_PROVIDER_PRIORITY_APPLICATION,
 };
 use gtk4_layer_shell::{Edge, Layer, LayerShell};
 use sysinfo::{DiskExt, System, SystemExt};
 
-pub struct Chunks {
-    app: Application,
+struct Factory {
+    application: Application,
 }
 
-impl Chunks {
-    pub fn new(application_id: &str) -> Self {
-        let app = Application::builder()
-            .application_id(application_id)
-            .build();
+struct Chunk;
 
-        Self { app }
+struct Internal;
+
+struct Wayland;
+
+#[derive(Debug, Clone, Copy)]
+struct EdgeConfig {
+    left: (Edge, bool),
+    right: (Edge, bool),
+    top: (Edge, bool),
+    bottom: (Edge, bool),
+}
+
+impl EdgeConfig {
+    const TOP_RIGHT: Self = Self {
+        left: (Edge::Left, false),
+        right: (Edge::Right, true),
+        top: (Edge::Top, true),
+        bottom: (Edge::Bottom, false),
+    };
+
+    const BOTTOM_RIGHT: Self = Self {
+        left: (Edge::Left, false),
+        right: (Edge::Right, true),
+        top: (Edge::Top, false),
+        bottom: (Edge::Bottom, true),
+    };
+
+    const TOP_LEFT: Self = Self {
+        left: (Edge::Left, true),
+        right: (Edge::Right, false),
+        top: (Edge::Top, true),
+        bottom: (Edge::Bottom, false),
+    };
+
+    const BOTTOM_LEFT: Self = Self {
+        left: (Edge::Left, true),
+        right: (Edge::Right, false),
+        top: (Edge::Top, false),
+        bottom: (Edge::Bottom, true),
+    };
+
+    const CENTER: Self = Self {
+        left: (Edge::Left, true),
+        right: (Edge::Right, true),
+        top: (Edge::Top, true),
+        bottom: (Edge::Bottom, true),
+    };
+
+    fn to_vec(&self) -> Vec<(Edge, bool)> {
+        vec![self.left, self.right, self.top, self.bottom]
+    }
+}
+
+impl Factory {
+    fn new(id: &str) -> Self {
+        gtk4::init().expect("Failed to initialize GTK");
+        let application = Application::builder().application_id(id).build();
+        Self { application }
     }
 
-    pub fn run(&self) -> ExitCode {
-        self.app.run()
+    fn pollute(&self, chunks: impl Fn(&Application) + 'static) -> ExitCode {
+        self.application.connect_activate(move |app| {
+            chunks(app);
+        });
+        self.application.run();
+        ExitCode::SUCCESS
     }
+}
 
-    pub fn create_window(&self, title: &str, child: &impl IsA<Widget>) -> ApplicationWindow {
-        ApplicationWindow::builder()
-            .application(&self.app)
-            .title(title)
-            .child(child)
-            .build()
-    }
-
-    pub fn setup_wayland_window(
-        &self,
-        window: &ApplicationWindow,
+impl Chunk {
+    fn new(
+        factory: &Application,
+        title: &str,
+        tag: Label,
         margins: Vec<(Edge, i32)>,
         anchors: Vec<(Edge, bool)>,
-        layout: Layer,
+        layer: Layer,
     ) {
-        window.init_layer_shell();
-        window.set_layer(layout);
+        let chunk = ApplicationWindow::builder()
+            .application(factory)
+            .title(title)
+            .child(&tag)
+            .build();
 
-        for (edge, margin) in margins {
-            window.set_margin(edge, margin)
+        if Wayland::detect_wayland() {
+            Wayland::setup_window(&chunk, margins, anchors, layer);
         }
 
-        for (anchor, state) in anchors {
-            window.set_anchor(anchor, state);
-        }
-
-        let (window_sender, window_receiver) = channel::<bool>();
-        self.event_listener(window_sender);
-
-        let window_weak = window.downgrade();
-        timeout_add_local(Duration::from_millis(100), move || {
-            if let Ok(is_fullscreen) = window_receiver.try_recv() {
-                if let Some(window) = window_weak.upgrade() {
-                    if is_fullscreen {
-                        window.hide();
-                    } else {
-                        window.show();
-                    }
-                }
-            }
-            gio::glib::ControlFlow::Continue
-        });
+        chunk.present();
     }
 
-    pub fn create_clock_widget(&self) -> Label {
-        let clock = Label::new(None);
-        clock.set_widget_name("clock_label");
-        self.handle_time(&clock);
-        clock
+    fn tag(class_name: &str) -> Label {
+        let tag = Label::new(None);
+        tag.set_widget_name(class_name);
+        tag
     }
+}
 
-    pub fn create_storage_widget(&self) -> Label {
-        let storage = Label::new(None);
-        storage.set_widget_name("storage_label");
-        self.handle_storage(&storage);
-        storage
-    }
-
-    pub fn load_css(&self, css: &str) {
-        let provider = CssProvider::new();
-        provider.load_from_data(css);
-
-        if let Some(display) = Display::default() {
-            style_context_add_provider_for_display(
-                &display,
-                &provider,
-                STYLE_PROVIDER_PRIORITY_APPLICATION,
-            );
-        }
-    }
-
-    fn event_listener(&self, window_sender: Sender<bool>) {
-        let instance_signature = env::var("HYPRLAND_INSTANCE_SIGNATURE")
-            .expect("HYPRLAND_INSTANCE_SIGNATURE not found. Is Hyprland running?");
-
-        let socket_path = format!("/run/user/1000/hypr/{}/.socket2.sock", instance_signature);
-
-        thread::spawn(move || {
-            if let Ok(mut stream) = UnixStream::connect(&socket_path) {
-                let _ = stream.write_all(b"subscribewindow\n");
-
-                let reader = BufReader::new(stream);
-                for line in reader.lines() {
-                    if let Ok(event) = line {
-                        if event.contains("fullscreen>>1") {
-                            let _ = window_sender.send(true);
-                        } else if event.contains("fullscreen>>0") {
-                            let _ = window_sender.send(false);
-                        }
-                    }
-                }
-            }
-        });
-    }
-
-    fn handle_time(&self, clock_label: &Label) {
+impl Internal {
+    fn handle_time(clock_label: &Label) {
         let clock_clone = clock_label.clone();
 
         let current_time = Local::now();
@@ -161,14 +151,14 @@ impl Chunks {
 
                 clock_clone.set_markup(&formatted_time);
 
-                ControlFlow::Continue
+                gio::glib::ControlFlow::Continue
             });
 
-            ControlFlow::Break
+            gio::glib::ControlFlow::Break
         });
     }
 
-    fn handle_storage(&self, storage_label: &Label) {
+    fn handle_storage(storage_label: &Label) {
         let storage_clone = storage_label.clone();
 
         let update_storage_usage = move || {
@@ -205,12 +195,86 @@ impl Chunks {
 
         timeout_add_seconds_local(60, update_storage_usage);
     }
+
+    fn load_css(style: &str) {
+        let provider = CssProvider::new();
+        provider.load_from_data(style);
+
+        if let Some(display) = Display::default() {
+            style_context_add_provider_for_display(
+                &display,
+                &provider,
+                STYLE_PROVIDER_PRIORITY_APPLICATION,
+            );
+        }
+    }
 }
 
-pub fn detect_wayland() -> bool {
-    let session_type = env::var("XDG_SESSION_TYPE").unwrap_or_default();
-    let wayland_display = env::var("WAYLAND_DISPLAY").unwrap_or_default();
+impl Wayland {
+    fn setup_window(
+        window: &ApplicationWindow,
+        margins: Vec<(Edge, i32)>,
+        anchors: Vec<(Edge, bool)>,
+        layout: Layer,
+    ) {
+        window.init_layer_shell();
+        window.set_layer(layout);
 
-    session_type.contains("wayland")
-        || (!wayland_display.is_empty() && !session_type.contains("x11"))
+        for (edge, margin) in margins {
+            window.set_margin(edge, margin)
+        }
+
+        for (anchor, state) in anchors {
+            window.set_anchor(anchor, state);
+        }
+
+        let (window_sender, window_receiver) = channel::<bool>();
+        Wayland::hypr_ipc(window_sender);
+
+        let window_weak = window.downgrade();
+        timeout_add_local(Duration::from_millis(100), move || {
+            if let Ok(is_fullscreen) = window_receiver.try_recv() {
+                if let Some(window) = window_weak.upgrade() {
+                    if is_fullscreen {
+                        window.hide();
+                    } else {
+                        window.show();
+                    }
+                }
+            }
+            gio::glib::ControlFlow::Continue
+        });
+    }
+
+    fn detect_wayland() -> bool {
+        let session_type = env::var("XDG_SESSION_TYPE").unwrap_or_default();
+        let wayland_display = env::var("WAYLAND_DISPLAY").unwrap_or_default();
+
+        session_type.contains("wayland")
+            || (!wayland_display.is_empty() && !session_type.contains("x11"))
+    }
+
+    fn hypr_ipc(window_sender: Sender<bool>) {
+        let instance_signature = env::var("HYPRLAND_INSTANCE_SIGNATURE")
+            .expect("HYPRLAND_INSTANCE_SIGNATURE not found. Is Hyprland running?");
+
+        let socket_path = format!("/run/user/1000/hypr/{}/.socket2.sock", instance_signature);
+
+        thread::spawn(move || {
+            if let Ok(mut stream) = UnixStream::connect(&socket_path) {
+                let _ = stream.write_all(b"subscribewindow\n");
+
+                let reader = BufReader::new(stream);
+                for line in reader.lines() {
+                    if let Ok(event) = line {
+                        if event.contains("fullscreen>>1") {
+                            let _ = window_sender.send(true);
+                        } else if event.contains("fullscreen>>0") {
+                            let _ = window_sender.send(false);
+                        }
+                    }
+                }
+            }
+        });
+    }
 }
