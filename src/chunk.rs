17f5c175e902835b4f1b6d7c7eeb0959591b843f
drkrssll <russellderek01@gmail.com>
2024-10-28 20:48:14 -0400

    Working on a picture widget

--- src/chunk.rs
+++ src/chunk.rs
@@ -1,25 +1,32 @@
-use gtk4::{prelude::GtkWindowExt, Application, ApplicationWindow, Label};
+use gio::prelude::Cast;
+use gtk4::{prelude::GtkWindowExt, Application, ApplicationWindow, Box, Label, Widget};
 use gtk4_layer_shell::{Edge, Layer};
 
 use crate::Wayland;
+
+pub enum Tag {
+    Label(Label),
+    Box(Box),
+}
 
 /// Represents a GTK4 window with a configuration for positioning/display on Wayland.
 /// The tag represents a text box with a CSS class name for styling.
 pub struct Chunk {
     factory: Application,
     title: String,
-    tag: Label,
+    tag: Tag,
     margins: Vec<(Edge, i32)>,
     anchors: Vec<(Edge, bool)>,
     layer: Layer,
 }
 
+/// Implementation of the `Chunk` struct.
 impl Chunk {
     /// Creates a new `Chunk` instance with the given parameters.
     pub fn new(
         factory: Application,
         title: String,
-        tag: Label,
+        tag: Tag,
         anchors: Vec<(Edge, bool)>,
         margins: Vec<(Edge, i32)>,
         layer: Layer,
@@ -36,10 +43,15 @@ impl Chunk {
 
     /// Builds and displays the `Chunk` window, configuring it for Wayland if detected.
     pub fn build(self) {
+        let child = match self.tag {
+            Tag::Label(label) => label.upcast::<Widget>(),
+            Tag::Box(box_) => box_.upcast::<Widget>(),
+        };
+
         let chunk = ApplicationWindow::builder()
             .application(&self.factory)
             .title(self.title)
-            .child(&self.tag)
+            .child(&child)
             .build();
 
         if Wayland::detect_wayland() {
