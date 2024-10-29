714d1787eab92f16ec892ee41622e76ff4dd30ed
drkrssll <russellderek01@gmail.com>
2024-10-28 20:57:17 -0400

    Working on a picture widget

--- src/internal.rs
+++ src/internal.rs
@@ -9,24 +9,29 @@ use gtk4::{glib::timeout_add_seconds_local, prelude::BoxExt, Box as GtkBox, Labe
 use regex::Regex;
 use sysinfo::{DiskExt, System, SystemExt};
 
+use crate::chunk::Tag;
+
 /// Collection of internal utilities for your widgets, including widget state management and data fetching.
 pub struct Internal;
 
 impl Internal {
-    /// Sets the static text of a GTK4 label, using markup if the text contains HTML-like tags.
-    pub fn static_widget(css_tag: &Label, text: String) {
-        if text.contains("</") && text.contains('>') {
-            css_tag.set_markup(&text);
-        } else {
-            css_tag.set_text(&text);
-        };
+    /// Sets the static text of a Widget, using markup if the text contains HTML-like tags.
+    pub fn static_widget(tag: &Tag, text: String) {
+        if let Tag::Label(label) = tag {
+            if text.contains("</") && text.contains('>') {
+                label.set_markup(&text);
+            } else {
+                label.set_text(&text);
+            }
+        }
     }
 
     /// Adds a picture to a GTK4 box, given a file path.
-    pub fn static_picture(box_tag: &GtkBox, pathname: &str) {
-        let picture = Picture::for_filename(pathname);
-
-        box_tag.append(&picture);
+    pub fn static_picture(tag: &Tag, pathname: &str) {
+        if let Tag::Box(box_tag) = tag {
+            let picture = Picture::for_filename(pathname);
+            box_tag.append(&picture);
+        }
     }
 
     /// Fetches the weather for a given location using the wttr.in API.
@@ -63,94 +68,92 @@ impl Internal {
     }
 
     /// Sets static text and then updates it at a given interval using a closure.
-    pub fn static_to_update<F, G>(
-        css_tag: &Label,
-        format_fn: F,
-        sleep: u32,
-        updated_fn: G,
-        interval: u32,
-    ) where
+    pub fn static_to_update<F, G>(tag: &Tag, format_fn: F, sleep: u32, updated_fn: G, interval: u32)
+    where
         F: Fn() -> String + 'static,
         G: Fn() -> String + 'static,
     {
-        let css_tag = css_tag.clone();
-        let css_updater = css_tag.clone();
-        let updated_fn = Arc::new(Mutex::new(updated_fn));
+        if let Tag::Label(label) = tag {
+            let css_tag = label.clone();
+            let css_updater = css_tag.clone();
+            let updated_fn = Arc::new(Mutex::new(updated_fn));
 
-        let update = move || {
-            let text = format_fn();
+            let update = move || {
+                let text = format_fn();
 
-            if text.contains("</") && text.contains('>') {
-                css_tag.set_markup(&text);
-            } else {
-                css_tag.set_text(&text);
-            };
-
-            ControlFlow::Continue
-        };
-
-        update();
-
-        timeout_add_seconds_local(sleep, move || {
-            let css_updater = css_updater.clone();
-            let updated_fn = Arc::clone(&updated_fn);
-
-            timeout_add_seconds_local(interval, move || {
-                let updated_text = (updated_fn.lock().unwrap())();
-
-                if updated_text.contains("</") && updated_text.contains('>') {
-                    css_updater.set_markup(&updated_text);
+                if text.contains("</") && text.contains('>') {
+                    css_tag.set_markup(&text);
                 } else {
-                    css_updater.set_text(&updated_text);
-                };
+                    css_tag.set_text(&text);
+                }
 
                 ControlFlow::Continue
-            });
+            };
 
-            ControlFlow::Break
-        });
+            update();
+
+            timeout_add_seconds_local(sleep, move || {
+                let css_updater = css_updater.clone();
+                let updated_fn = Arc::clone(&updated_fn);
+
+                timeout_add_seconds_local(interval, move || {
+                    let updated_text = (updated_fn.lock().unwrap())();
+
+                    if updated_text.contains("</") && updated_text.contains('>') {
+                        css_updater.set_markup(&updated_text);
+                    } else {
+                        css_updater.set_text(&updated_text);
+                    }
+
+                    ControlFlow::Continue
+                });
+
+                ControlFlow::Break
+            });
+        }
     }
 
     /// Updates a GTK4 label using a closure at a given interval.
-    pub fn update_widget<F>(css_tag: &Label, format_fn: F, interval: u32)
+    pub fn update_widget<F>(tag: &Tag, format_fn: F, interval: u32)
     where
         F: Fn() -> String + 'static,
     {
-        let css_tag = css_tag.clone();
+        if let Tag::Label(label) = tag {
+            let css_tag = label.clone();
 
-        let update = move || {
-            let text = format_fn();
+            let update = move || {
+                let text = format_fn();
 
-            if text.contains("</") && text.contains('>') {
-                css_tag.set_markup(&text);
-            } else {
-                css_tag.set_text(&text);
+                if text.contains("</") && text.contains('>') {
+                    css_tag.set_markup(&text);
+                } else {
+                    css_tag.set_text(&text);
+                }
+
+                ControlFlow::Continue
             };
 
-            ControlFlow::Continue
-        };
+            update();
 
-        update();
-
-        timeout_add_seconds_local(interval, update);
+            timeout_add_seconds_local(interval, update);
+        }
     }
 
     /// Updates the time in a GTK4 label every second given a closure.
-    pub fn update_time<F>(css_tag: &Label, format_fn: F)
+    pub fn update_time<F>(tag: &Tag, format_fn: F)
     where
         F: Fn() -> String + 'static,
     {
-        Internal::update_widget(css_tag, format_fn, 1)
+        Internal::update_widget(tag, format_fn, 1);
     }
 
     /// Updates the storage in a GTK4 label every 2 minutes given a closure.
-    pub fn update_storage<F>(css_tag: &Label, format_fn: F)
+    pub fn update_storage<F>(tag: &Tag, format_fn: F)
     where
         F: Fn() -> String + 'static,
     {
-        Internal::update_widget(css_tag, format_fn, 120)
+        Internal::update_widget(tag, format_fn, 120);
     }
-
     /// Fetches the current storage usage as a percentage.
     pub fn get_storage() -> f64 {
         let mut system = System::new_all();
